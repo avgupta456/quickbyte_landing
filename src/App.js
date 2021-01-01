329696ef10d3e3e319c5b2035948e57840ff774e
Abhijit Gupta <avgupta456@gmail.com>
2021-01-01 16:03:03 -0500

    apply background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import Background from './Background.js';
+
 function App() {
   return (
-    <div className="bg-gray-50">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Ready to dive in?</span>
-          <span className="block text-indigo-600">Start your free trial today!</span>
-        </h2>
-        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-          <a href="https://www.google.com" className="px-5 py-3 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">Get Started</a>
-          <a href="https://www.google.com" className="ml-3 px-5 py-3 font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">Learn More</a>
-        </div>
+    <div>
+      <Background />
+      <div className="absolute w-screen h-screen">
+        <h2 className="text-7xl font-bold text-gray-50">QuickBites</h2>
       </div>
     </div>
   );
