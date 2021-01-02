09d7ebb2ea5aaa13a07795f0388f91635f5c602d
Abhijit Gupta <avgupta456@gmail.com>
2021-01-02 18:41:45 -0500

    Create Privacy Policy

--- src/Privacy.js
+++ src/Privacy.js
@@ -1,7 +1,427 @@
 import React from 'react';
 
 const Privacy = () => {
-  return <h2>Privacy</h2>;
+  return (
+    <div className="w-3/4 mx-auto mt-4 px-4 py-4 rounded shadow">
+      <h1 className="text-3xl font-bold">PRIVACY NOTICE</h1>
+      <h2 className="text-xl">Last Updated: Jan 01, 2021</h2>
+      <br />
+      <p>
+        Thank you for choosing QuickBites (“Company”, “we”, “us”, “our”). We are
+        committed to protecting your personal information and your right to
+        privacy. If you have any questions or concerns about this privacy
+        notice, or our practices with regards to your personal information,
+        please contact us at{' '}
+        <a
+          href="mailto:avgupta456@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 underline"
+        >
+          avgupta456@gmail.com
+        </a>
+        .
+      </p>
+      <br />
+      <p>
+        When you use our mobile application, as the case may be (the “App”) and
+        more generally, use any of our services (the “Services”, which include
+        the App), we appreciate that you are trusting us with your personal
+        information. We take your privacy very seriously. In this privacy
+        notice, we seek to explain to you in the clearest way possible what
+        information we collect, how we use it and what rights you have in
+        relation to it. We hope you take some time to read through it carefully,
+        as it is important. If there are any terms in this privacy notice that
+        you do not agree with, please discontinue use of our Services
+        immediately.
+      </p>
+      <br />
+      <p>
+        This privacy notice applies to all information collected through our
+        Services (which, as described above, include our App), as well as, any
+        related services, sales, marketing or events.
+      </p>
+      <br />
+      <p className="font-bold">
+        Please read this privacy notice carefully as it will help you understand
+        what we do with the information we collect.
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">1. WHAT INFORMATION DO WE COLLECT?</h3>
+      <br />
+      <h4 className="text-xl font-bold">Information automatically collected</h4>
+      <br />
+      <p>
+        <b>In short</b>: Some information – such as your Internet Protocol (IP)
+        address and/or browser and device characteristics – is collected
+        automatically when you visit our App.
+      </p>
+      <br />
+      <p>
+        We automatically collect certain information when you visit, use or
+        navigate the App. This information does not reveal your specific
+        identity (like your name or contact information) but may include device
+        and usage information, such as your IP address, browser and device
+        characteristics, operating system, language preferences, referring URLs,
+        device name, country, location, information about how and when you use
+        our App and other technical information. This information is primarily
+        needed to maintain the security and operation of our App, and for our
+        internal analytics and reporting purposes.
+      </p>
+      <br />
+      <p>The information we collect includes:</p>
+      <ul className="list-disc ml-8">
+        <li>
+          <b>Log and Usage Data</b>. Log and usage data is service-related,
+          diagnostic, usage and performance information our servers
+          automatically collect when you access or use our App and which we
+          record in log files. Depending on how you interact with us, this log
+          data may include your IP address, device information, browser type and
+          settings and information about your activity in the App (such as the
+          date/time date/time stamp associated with your usage, pages and files
+          viewed, searches and other actions you take such as which features you
+          use), device event information (such as system activity, error reports
+          ( sometimes called ‘crash dumps’) and hardware settings).
+        </li>
+        <li>
+          <b>Device Data</b>. We collect device data such as information about
+          your computer, phone, tablet or other device you use to access the
+          App. Depending on the device used, this device data may include
+          information such as your IP address (or proxy server), device and
+          application identification numbers, location, browser type, hardware
+          model Internet service provider and/or mobile carrier, operating
+          system and system configuration information.
+        </li>
+        <li>
+          <b>Location Data</b>. We collect location data such as information
+          about your device’s location, which can be either precise or
+          imprecise. How much information we collect depends on the type and
+          settings of the device you use to access the App. For example, we may
+          use GPS and other technologies to collect geolocation data that tells
+          us your current location (based on your IP address). You can opt out
+          of allowing us to collect this information either by refusing access
+          to the information or by disabling your Location setting on your
+          device. Not however, if you choose to opt out, you may not be able to
+          use certain aspects of the Services.
+        </li>
+      </ul>
+      <br />
+      <h4 className="text-xl font-bold">
+        Information collected through our App
+      </h4>
+      <br />
+      <p>
+        <b>In short</b>: We collect information regarding your geo-location,
+        mobile device, when you use our App.
+      </p>
+      <br />
+      <p>If you use our App, we also collect the following information:</p>
+      <ul className="list-disc ml-8">
+        <li>
+          <b>Geo-Location Information</b>. We may request access or permission
+          to and track location-based information from our mobile device, either
+          continuously or while you are using our App, to provide certain
+          location-based services. If you wish to change our access or
+          permissions, you may do so in your device’s settings.
+        </li>
+        <li>
+          <b>Mobile Device Data</b>. We automatically collect device information
+          (such as your mobile device ID, model and manufacturer), operating
+          system, version information and system configuration information,
+          device and application identification numbers, browser type and
+          version, hardware model Internet service provider and/or mobile
+          carrier, and Internet (IP) address (or proxy server). If you are using
+          our App, we may also collect information about the phone network
+          associated with your mobile device, your mobile device’s operating
+          system or platform, the type of mobile device you use, your mobile
+          device’s unique device ID and information about the features of our
+          App you accessed.
+        </li>
+      </ul>
+      <br />
+      <p>
+        This information is primarily needed to maintain the security and
+        operation of our App, for troubleshooting and for our internal analytics
+        and reporting purposes.
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        2. WILL YOUR INFORMATION BE SHARED WITH ANYONE?
+      </h3>
+      <br />
+      <p>
+        <b>In Short:</b> We only share information with your consent, to comply
+        with laws, to provide you with services, to protect your rights, or to
+        fulfill business obligations.
+      </p>
+      <br />
+      <p>
+        We may process or share your data that we hold based on the following
+        legal basis:
+      </p>
+      <ul className="list-disc ml-8">
+        <li>
+          <b>Consent</b>: We may process your data if you have given us specific
+          consent to your personal information for a specific purpose
+        </li>
+        <li>
+          <b>Legitimate Interests</b>: We may process your data when it is
+          reasonably necessary to achieve our legitimate business interests.
+        </li>
+        <li>
+          <b>Performance of a Contract</b>: Where we have entered into a
+          contract with you, we may process your personal information to fulfill
+          the terms of our contract.
+        </li>
+        <li>
+          <b>Legal Obligations</b>: We may disclose your information where we
+          are legally required to do so in order to comply with applicable law,
+          governmental requests, a judicial proceeding, court order, or legal
+          process, such as in response to a court order or a subpoena (including
+          in response to public authorities to meet national security or law
+          enforcement requirements).
+        </li>
+        <li>
+          <b>Vital Interests</b>: We may disclose your information where we
+          believe it is necessary to investigate, prevent, or take action
+          regarding potential violations of our policy, suspected fraud,
+          situations involving potential threats to the safety of any person and
+          illegal activities, or as evidence in litigation in which we are
+          involved.
+        </li>
+      </ul>
+      <br />
+      <p>
+        More specifically, we may need to process your data or share your
+        personal information in the following situations:
+      </p>
+      <ul className="list-disc ml-8">
+        <li>
+          <b>Business Transfers</b>. We may share or transfer your information
+          in connection with, or during negotiations of, any merger, sale or
+          company assets, financing, or acquisition of all or a portion of our
+          business to another company.
+        </li>
+      </ul>
+      <br />
+      <h3 className="text-2xl font-bold">
+        3. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?
+      </h3>
+      <br />
+      <p>
+        <b>In Short:</b> We may transfer, store, and process your information in
+        countries other than your own.
+      </p>
+      <br />
+      <p>
+        Our servers are located in the United States. If you are accessing our
+        App from outside, please be aware that your information may be
+        transferred to, stored, and processed by using our facilities and by
+        those third parties with whom we may share your personal information, in
+        the United States and other countries.
+      </p>
+      <br />
+      <p>
+        If you are a resident in the European Economic Area, then these
+        countries may not necessarily have data protection laws or other similar
+        laws as comprehensive as those in your country. We will however take all
+        necessary measures to protect your personal information in accordance
+        with this privacy notice and applicable laws.
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        4. HOW LONG DO WE KEEP YOUR INFORMATION?
+      </h3>
+      <p>
+        <b>In Short:</b> We keep your information for as long as necessary to
+        fulfill the purposes outlined in this privacy notice unless otherwise
+        required by law.
+      </p>
+      <br />
+      <p>
+        We will only keep your personal information for as long as it is
+        necessary for the purposes set out in this privacy notice, unless a
+        longer retention period is required or permitted by law (such as tax,
+        accounting or other legal requirements). No purpose in this notice will
+        require us keeping your personal information for longer than 90 days.
+      </p>
+      <br />
+      <p>
+        When we have no ongoing legitimate business need to process your
+        personal information, we will either delete or anonymize such
+        information, or, if this is not possible ( for example, because your
+        personal information has been stored in backup archives), then we will
+        securely store your personal information and isolate it from any further
+        processing until deletion is possible.
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        5. HOW DO WE KEEP YOUR INFORMATION SAFE?
+      </h3>
+      <br />
+      <p>
+        <b>In Short:</b> We aim to protect your personal information through a
+        system of organizational and technical security measures.
+      </p>
+      <br />
+      <p>
+        We have implemented appropriate technical and organizational security
+        measures designed to protect the security of any personal information we
+        process. However, despite our safeguards and efforts to secure your
+        information, no electronic transmission over the Internet or information
+        storage technology can be guaranteed to be 100% secure, so we cannot
+        promise or guarantee that hackers, cybercriminals, or other unauthorized
+        third parties will not be able to defeat our security, and improperly
+        collect, access, steal, or modify your information. Although we will do
+        our best to protect your personal information, transmission of personal
+        information to and from our App is at your own risk. You should only
+        access the App within a secure environment.
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">6. WHAT ARE YOUR PRIVACY RIGHTS?</h3>
+      <br />
+      <p>
+        <b>In Short:</b> You may review, change, or terminate your account at
+        any time.
+      </p>
+      <br />
+      <p>
+        If you are a resident in the European Economic Area and you believe we
+        are unlawfully processing your personal information, you also have the
+        right to complain to your local data protection supervisory authority.
+        You can find their contact details here:{' '}
+        <a
+          href="http://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 underline"
+        >
+          http://ec.europa.eu/justice/data-protection/bodies/authorities/index_en.htm
+        </a>
+        .
+      </p>
+      <br />
+      <p>
+        If you are a resident in Switzerland, the contact detail for the data
+        protection authorities are available here:{' '}
+        <a
+          href="https://www.edoeb.admin.ch/edoeb/en/home.html"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 underline"
+        >
+          https://www.edoeb.admin.ch/edoeb/en/home.html
+        </a>
+        .
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        7. CONTROLS FOR DO-NOT-TRACK FEATURES
+      </h3>
+      <br />
+      <p>
+        Most web browsers and some mobile operating systems and mobile
+        applications include a Do-Not-Track (“DNT”) feature or setting you can
+        activate to signal your privacy preference not to have data about your
+        online browsing activities monitored and collected. At this stage no
+        uniform technology standard for recognizing and implementing DNT signals
+        has been finalized. As such, we do not currently respond to DNT browser
+        signals or any other mechanisms that automatically communicates your
+        choice not to be tracked online. If a standard online tracking system is
+        adopted that we must follow in the future, we will inform you about that
+        practice in a revised version of this privacy notice.
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        8. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
+      </h3>
+      <br />
+      <p>
+        <b>In Short:</b> Yes, if you are a resident of California, you are
+        granted specific rights regarding access to your personal information.
+      </p>
+      <br />
+      <p>
+        California Civic Code Section 1798.83, also known as the “Shine the
+        Light” law, permits our users who are California residents to request
+        and obtain from us, once a year and free of charge, information about
+        categories of personal information (if any) we disclosed to third
+        parties for direct marketing purposes and the names and addresses of all
+        third parties with which we shared personal information in the
+        immediately preceding calendar year. If you are a California resident
+        and would like to make such a request, please submit your request in
+        writing to us using the contact information provided below.
+      </p>
+      <br />
+      <p>
+        If you are under 18 years of age, reside in California, and have a
+        registered account with the App, you have the right to request removal
+        of unwanted data that you publicly post on the App. To request removal
+        of such data, please contact us using the contact information provided
+        below, and include the email address associated with your account and a
+        statement that you reside in California. We will make sure the data is
+        not publicly displayed on the App, but please be aware that the data may
+        not be completely or comprehensively removed from all our systems (e.g.
+        backups, etc.).
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        9. DO WE MAKE UPDATES TO THIS NOTICE?
+      </h3>
+      <br />
+      <p>
+        <b>In Short:</b> Yes, we will update this notice as necessary to stay
+        compliant with relevant laws.
+      </p>
+      <br />
+      <p>
+        We may update this privacy notice from time to time. The updated version
+        will be indicated by an updated “Revsied” date and the updated version
+        will be effective as soon as it is accessible. IF we make material
+        changes to this privacy notice, we may notify you either by prominently
+        posting a notice of such changes or by directly sending you a
+        notification. We encourage you to review this privacy notice frequently
+        to be informed of how we are protecting your information.
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        10. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?
+      </h3>
+      <br />
+      <p>
+        If you have questions or comments about this notice, you may email us at{' '}
+        <a
+          href="mailto:avgupta456@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 underline"
+        >
+          avgupta456@gmail.com
+        </a>
+      </p>
+      <br />
+      <h3 className="text-2xl font-bold">
+        11. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA THAT WE COLLECT FROM
+        YOU?
+      </h3>
+      <br />
+      <p>
+        Based on the applicable laws of your country, you may have the right to
+        request access to the personal information we collect from you, change
+        that information, or delete it in some circumstances. To request a
+        review, update, or delete your personal information, please email us at{' '}
+        <a
+          href="mailto:avgupta456@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 underline"
+        >
+          avgupta456@gmail.com
+        </a>
+        . We will respond to your request within 30 days.
+      </p>
+    </div>
+  );
 };
 
 export default Privacy;
