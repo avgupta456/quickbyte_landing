bc02d8cee1e8d4411b3fc97e0df1340ee3d0492e
Abhijit Gupta <avgupta456@gmail.com>
2021-01-02 00:14:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ import Terms from './Terms';
 const Footer = () => {
   return (
     <div className="absolute h-screen w-screen overflow-x-hidden">
-      <div className="absolute inset-x-0 bottom-2 text-center text-gray-500 text-lg">
-        © 2021.{' '}
+      <div className="absolute inset-x-0 bottom-2 text-center text-gray-400 text-lg">
+        © 2021. By{' '}
         <a
           href="https://www.abhijitgupta.io"
-          className="hover:text-red-500"
+          className="hover:text-red-500 underline"
           target="_blank"
           rel="noreferrer"
         >
           Abhijit Gupta.
         </a>{' '}
-        <Link to="/privacy" className="hover:text-red-500">
+        <Link to="/privacy" className="hover:text-red-500 underline">
           Privacy Policy
         </Link>
         .{' '}
-        <Link to="/terms" className="hover:text-red-500">
+        <Link to="/terms" className="hover:text-red-500 underline">
           Terms of Service
         </Link>
       </div>
