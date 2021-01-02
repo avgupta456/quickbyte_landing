9f7c29883b1361c1d46c12e44bfe7df1bfed7c28
Abhijit Gupta <avgupta456@gmail.com>
2021-01-02 18:41:55 -0500

    Create Terms and Conditions

--- src/Terms.js
+++ src/Terms.js
@@ -1,7 +1,89 @@
 import React from 'react';
 
 const Terms = () => {
-  return <h2>Terms</h2>;
+  return (
+    <div className="w-3/4 mx-auto mt-4 px-4 py-4 rounded shadow">
+      <h1 className="text-3xl font-bold">TERMS AND CONDITIONS</h1>
+      <h2 className="text-xl">Last Updated: Jan 01, 2021</h2>
+      <br />
+      <h4 className="text-xl font-bold">Terms and Conditions</h4>
+      <br />
+      <p>
+        These terms and conditions (“Agreement”) set forth the general terms and
+        conditions of your use of the “QuickBites” mobile application (“Mobile
+        Application” or “Service”) and any of its related products and services
+        (collectively, “Services”). This Agreement is legally binding between
+        you (“User”, “you” or “your”) and this Mobile Application developer
+        (“Operator”, “we”, “us” or “our”). By accessing and using the Mobile
+        Application and Services, you acknowledge that you have read,
+        understood, and agree to be bound by the terms of this Agreement. If you
+        are entering into this Agreement on behalf of a business or other legal
+        entity, you represent that you have the authority to bind such entity to
+        this Agreement, in which case the terms “User”, “you” or “your” shall
+        refer to such entity. If you do not have such authority, or if you do
+        not agree with the terms of this Agreement, you must not accept this
+        Agreement and may not access and use the Mobile Application and
+        Services. You acknowledge that this Agreement is a contract between you
+        and the Operator, even though it is electronic and is not physically
+        signed by you, and it governs your use of the Mobile Application and
+        Services.
+      </p>
+      <br />
+      <h4 className="text-xl font-bold">Links to Other Resources</h4>
+      <br />
+      <p>
+        Although the Mobile Application and Services may link to other resources
+        (such as websites, mobile applications, etc.), we are not, directly or
+        indirectly, implying any approval, association, sponsorship,
+        endorsement, or affiliation with any linked resource, unless
+        specifically stated herein. We are not responsible for examining or
+        evaluating, and we do not warrant the offerings of, any businesses or
+        individuals or the content of their resources. We do not assume any
+        responsibility or liability for the actions, products, services, and
+        content of any other third parties. You should carefully review the
+        legal statements and other conditions of use of any resource which you
+        access through a link in the Mobile Application and Services. Your
+        linking to any other off-site resources is at your own risk.
+      </p>
+      <br />
+      <h4 className="text-xl font-bold">Changes and Ammendments</h4>
+      <br />
+      <p>
+        We reserve the right to modify this Agreement or its terms relating to
+        the Mobile Application and Services at any time, effective upon posting
+        of an updated version of this Agreement in the Mobile Application. When
+        we do, we will revise the updated date at the bottom of this page.
+        Continued use of the Mobile Application and Services after any such
+        changes shall constitute your consent to such changes.
+      </p>
+      <br />
+      <h4 className="text-xl font-bold">Acceptance of these Terms</h4>
+      <br />
+      <p>
+        You acknowledge that you have read this Agreement and agree to all its
+        terms and conditions. By accessing and using the Mobile Application and
+        Services you agree to be bound by this Agreement. If you do not agree to
+        abide by the terms of this Agreement, you are not authorized to access
+        or use the Mobile Application and Services.
+      </p>
+      <br />
+      <h4 className="text-xl font-bold">Contacting Us</h4>
+      <br />
+      <p>
+        If you would like to contact us to understand more about this Agreement
+        or wish to contact us concerning any matter relating to it, you may send
+        an email to{' '}
+        <a
+          href="mailto:avgupta456@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 underline"
+        >
+          avgupta456@gmail.com
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Terms;
