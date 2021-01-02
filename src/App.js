ab2e91bff3d61bed0cb862e57cf21dc5cb45009a
Abhijit Gupta <avgupta456@gmail.com>
2021-01-01 23:29:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Background from './Background.js';
 
 function App() {
@@ -5,7 +7,24 @@ function App() {
     <div>
       <Background />
       <div className="absolute w-screen h-screen">
-        <h2 className="text-7xl font-bold text-gray-50">QuickBites</h2>
+        <div className="flex justify-around">
+          <div className="mt-32 content-center">
+            <h2 className="text-6xl lg:text-8xl font-bold text-red-500 lg:text-white lg:hover:text-red-50 text-center">
+              QuickBites
+            </h2>
+            <h4 className="text-2xl lg:text-4xl font-bold text-red-500 lg:text-white lg:hover:text-red-50 mt-8 text-center">
+              All-in-One Food-Delivery Search
+            </h4>
+            <div className="flex mt-8 justify-center space-x-4">
+              <img src="apple.png" alt="" className="h-20" />
+              <img src="google.png" alt="" className="h-20" />
+            </div>
+          </div>
+          <div className="flex justify-center right mt-24">
+            <img src="iphone.png" alt="" className="w-96" />
+            <img src="iphone.png" alt="" className="w-96" />
+          </div>
+        </div>
       </div>
     </div>
   );
